e80ec9eae2f560f8d6d4134ada6ab408d83eeb2b
Pham Duc Anh <banhbabanhmj123@gmail.com>
2024-08-14 20:56:52 +0700

    Bài ghi trên lớp 14.8

---
+++ BT_/BT_1+2/script.js
@@ -0,0 +1,71 @@
+// bài 1 :
+const handelClick = document.getElementById("title")
+handelClick.innerHTML = "Hello, DOM!."
+
+
+// bài 2 :
+const handelClick2 = document.getElementById("text")
+handelClick2.style.color = "red"
+
+
+
+// bài 4 :
+
+const handelClick4 = document.getElementById("remove").remove()
+// console.log(handelClick4)
+
+
+// bài 9 :
+const EleDiv = document.querySelectorAll('div');
+const divCount = EleDiv.length;
+
+console.log(`Số lượng phần tử <div> trên trang là: ${divCount}`); 
+
+
+// bài 3 :
+const list = document.querySelector('#list');
+
+const newListItem = document.createElement("li");
+newListItem.textContent = "Mục mới"; 
+list.appendChild(newListItem);
+
+
+// bài 10 :
+const handelClick10 = document.querySelectorAll('.item');
+
+// handelClick10.forEach(item => {
+//   item.textContent = 'Updated item';
+// });
+handelClick10.map((item) => {
+    return(
+        item.classList.replace('item', 'updatedItem')
+    )
+} )
+// bài 8 
+const tableContainer = document.querySelector("#table-container");
+
+const table = document.createElement("table");
+
+for (let i = 0; i < 3; i++) {
+  const row = table.insertRow()
+  for (let j = 0; j < 3; j++) {
+    const cell = row.insertCell()
+    cell.textContent = `Hàng ${i + 1}, Cột ${j + 1}`
+  }
+}
+
+tableContainer.appendChild(table);
+
+
+// bài 5
+    function changeImageSrc() {
+    const handelClick5 = document.getElementById('image');
+    
+    handelClick5.src = 'path/to/new-image.jpg';
+}
+
+// bài 7
+const handelClick7 = document.querySelectorAll("p");
+handelClick7.forEach(item => {
+  item.textContent = "Updated paragraph";
+});
---
+++ BT_/BT_6/script.js
@@ -0,0 +1,8 @@
+// const handleClick = () => {
+//     alert("Click Me")
+// }
+const handleClick2 = document.getElementById("btn")
+console.log(handleClick2)
+handleClick2.addEventListener('click' , () => {
+    alert("Click Me")
+})
